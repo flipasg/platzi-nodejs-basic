b7a63b1514f47ecc1807d83bc1e9924ecdeae82b
Iker Garcia Ramirez <ikergarciadev@gmail.com>
2023-07-20 15:10:33 +0200

    feat(modules): :sparkles: add console usage (13)

---
+++ modules/console.js
@@ -0,0 +1,36 @@
+console.log('Algo');
+console.info('Algo');
+console.error('Algo');
+console.warn('Algo');
+console.table([1, 2, 3, 4, 5]);
+console.table([{
+  a: 1,
+  b: 'Z'
+}, {
+  a: 'A',
+  b: 2
+}]);
+console.group('Conversación');
+console.log('Hola');
+console.group('Bla');
+console.log('Bla bla bla');
+console.log('Bla bla bla');
+console.log('Bla bla bla');
+console.groupEnd('Bla');
+console.log('Adios');
+console.groupEnd('Conversación');
+console.log('Otra cosa de otra función');
+console.count('veces');
+console.count('veces');
+console.count('veces');
+console.countReset('veces');
+console.count('veces');
+console.time('Tiempo');
+console.log('Hola');
+console.log('Hola');
+console.log('Hola');
+console.log('Hola');
+console.log('Hola');
+console.log('Hola');
+console.timeEnd('Tiempo');
+
