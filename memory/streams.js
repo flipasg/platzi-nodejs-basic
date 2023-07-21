c4c45fefae420ed8cc595375b7c630b31cbbece8
Iker Garcia Ramirez <ikergarciadev@gmail.com>
2023-07-21 12:08:36 +0200

    feat(memory): :sparkles: add streams with streams package (25)

--- memory/streams.js
+++ memory/streams.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
+const stream = require('stream');
+const util = require('util');
 
 let data = '';
 const rs = fs.createReadStream(__dirname + '/stream.txt');
@@ -8,3 +10,20 @@ rs.on('data', (chunk) => {
 rs.on('end', () => {
   console.log(data);
 });
+
+process.stdout.write('Hello ');
+
+const Transform = stream.Transform;
+function Mayus() {
+  Transform.call(this);
+}
+util.inherits(Mayus, Transform);
+
+Mayus.prototype._transform = function (chunk, codif, cb) {
+  chunkMayus = chunk.toString().toUpperCase();
+  this.push(chunkMayus);
+  cb();
+};
+
+const mayus = new Mayus();
+rs.pipe(mayus).pipe(process.stdout);
